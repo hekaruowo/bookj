59ffa6c8d9f19eeac92e0fc0c443a6eaca0015a9
Nikita <123164416+hekaruowo@users.noreply.github.com>
2025-06-18 17:33:12 +0300

    Списки и взаимодействия с ними

---
+++ bicycles.py
@@ -0,0 +1,7 @@
+from simple_message import message
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+# print(bicycles)
+print(bicycles[0].title())
+message = f"My first bicycle was a {bicycles[-1].title()}"
+print(message)
---
+++ names.py
@@ -0,0 +1,11 @@
+names = [' clown ', ' negr ',' Egorka ',' Ya ']
+print(names[0].strip().title())
+print(names[1].strip().title())
+print(names[2].strip().title())
+print(names[3].strip().title())
+print(f"Hello {names[0].strip().title()}")
+print(f"Hello {names[1].strip().title()}")
+print(f"Hello {names[2].strip().title()}")
+print(f"Hello {names[3].strip().title()}")
+cars = ['GTR R35', 'LANDCRUISER']
+print(f'I wanna buy {cars[0]} and {cars[1]}.')
