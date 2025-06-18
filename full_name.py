7c146fce89b8fde1723b837bc9f17d723eadc5e5
Nikita <123164416+hekaruowo@users.noreply.github.com>
2025-06-18 15:03:51 +0300

    4

--- full_name.py
+++ full_name.py
@@ -5,3 +5,17 @@ print(full_name)
 print(f"Hello, {full_name.title()}!")
 message = f"Hello, {full_name.title()}!"
 print(message)
+language = " Python "
+print(language)
+language = language.rstrip()
+print(language)
+language = language.lstrip()
+print(language)
+language = " Python "
+print(language)
+language = language.strip()
+print(language)
+url_1 = "https://www.google.com"
+print(url_1)
+url_1 = url_1.removeprefix("https://")
+print(url_1)
