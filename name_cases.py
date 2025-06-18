db1aa95b746d50beb89195e92c6338206269fdf4
Nikita <123164416+hekaruowo@users.noreply.github.com>
2025-06-18 15:24:42 +0300

    6

---
+++ name_cases.py
@@ -0,0 +1,20 @@
+
+name = input("Введите ваше имя: ")
+name = name.strip()
+name = name.title()
+print("Здравствуйте,", name,",не хотите ли вы изучить Python сегодня?")
+print(name.upper())
+print(name.lower())
+print(name.title())
+print('Альберт Эйнштейн однажды сказал: "Тот, кто никогда не совершал ошибок, никогда не пробовал ничего нового".')
+famous_person = "Альберт Эйнштейн"
+print(famous_person, 'однажды сказал: "Тот, кто никогда не совершал ошибок, никогда не пробовал ничего нового".')
+famous_person2 = "Альберт Эйнштейн\tАльберт Эйнштейн\nАльберт Эйнштейн\tАльберт Эйнштейн\nАльберт Эйнштейн    "
+print(famous_person2)
+famous_person2 = famous_person2.title()
+print(famous_person2)
+famous_person2 = famous_person2.strip()
+print(famous_person2)
+filename = "python_notes.txt"
+filename =  filename.removesuffix(".txt")
+print(filename)
