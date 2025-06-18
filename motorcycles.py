731be92fc9cbee71ddc5530693650343f7890135
Nikita <123164416+hekaruowo@users.noreply.github.com>
2025-06-18 18:22:37 +0300

    Списки и взаимодействия с ними Добавление элементов в конец и запись в определенный номер

---
+++ motorcycles.py
@@ -0,0 +1,13 @@
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+motorcycles[0] = 'ducati'
+print(motorcycles)
+motorcycles.append('honda')
+print(motorcycles)
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+motorcycles.append('ducati')
+print(motorcycles)
